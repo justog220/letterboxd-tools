7f1b199bd449f3e7787701ea872c24b5a660016c
justog220 <justogarciapc@gmail.com>
2023-12-12 19:56:46 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,138 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Center
+from textual.widgets import Tabs, TabPane, Header, TabbedContent, Input, Markdown, Tab, Static, Button, ProgressBar, LoadingIndicator
+from modulos.main import get_list_content, get_user_watchlist, existe_usuario, existe_lista
+from textual import on
+from random import choice
+
+
+class Application(App[None]):
+    CSS_PATH = "styles/application.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        with TabbedContent(id="tabs"):
+            with TabPane(":person_standing: Watchlists", id="watchlists"):
+                yield UserInput()
+            with TabPane(":film_frames: Lista"):
+                yield ListInput()
+        # yield Roulette()
+
+
+    def on_mount(self) -> None:
+        self.title = "Letterboxd Tools"
+
+class UserInput(Static):
+    def __init__(self):
+        super().__init__()
+        self.usuarios = []
+        self.input_widget = Input(placeholder="Inserte un usuario", id="user-input")
+        self.custom_button = Button("Ir a ruleta", id="button-roulette")
+        self.roulette = Roulette()
+        self.roulette.display = False
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield self.input_widget
+        with Center():
+            yield self.custom_button
+        with Center():
+            yield self.roulette
+
+
+    # @on(Input.Submitted)
+    # def store_user(self):
+    #     with open("usuarios.txt", "w") as archi:
+    #         archi.write(Input.Submitted.value)
+
+    def on_input_submitted(self, subm):
+        if subm.value != "" and existe_usuario(subm.value):
+            self.usuarios.append(subm.value)
+            self.input_widget.clear()
+            self.notify("Usuario agregado", timeout=1)
+        else:
+            self.notify("Usuario no existente", timeout=1, severity="error")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if self.usuarios != []:
+            if event.button.id == "button-roulette":
+                self.input_widget.display = False
+                self.custom_button.display = False
+                self.roulette.set_users(self.usuarios)
+                self.roulette.display = True
+        else:
+            self.notify("Inserte por lo menos un usuario", severity="error")
+
+
+class ListInput(Static):
+    def __init__(self):
+        super().__init__()
+        self.url = None
+        self.input_widget = Input(placeholder="Inserte una lista", id="list-input")
+        self.roulette = Roulette()
+        self.roulette.display = False
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield self.input_widget
+        with Center():
+            yield self.roulette
+
+    def on_input_submitted(self, subm):
+        if subm.value != "" and existe_lista(subm.value):
+            self.url = subm.value
+            self.input_widget.clear()
+            self.notify("Lista registrada", timeout=1)
+            self.input_widget.display = False
+            self.roulette.set_list(self.url)
+            self.roulette.display = True
+        else:
+            self.notify("Lista inexistente", timeout=1, severity="error")
+
+
+    
+
+class Roulette(Static):
+    def __init__(self, list_url=None, users=[]):
+        super().__init__()
+        self.films_list = []
+        self.film_displayed = ""
+        self.film_title = Static(renderable="[b]"+self.film_displayed, id="film-title")
+        self.progress_bar = ProgressBar(total=0, show_eta=False)
+        self.watched_button = Button(":eye:", id="watched-button")
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield self.film_title
+        with Center():
+            yield self.watched_button
+        with Center():
+            yield self.progress_bar
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "watched-button":
+            self.films_list.remove(self.film_displayed)
+            self.film_displayed = choice(self.films_list)
+            self.film_title.update("[b]"+self.film_displayed)
+            self.progress_bar.advance(1)
+
+    def set_users(self, users):
+        for user in users:
+            self.films_list += get_user_watchlist(user)
+        self.largo_inicial = len(self.films_list)
+        self.vistas = 0
+        self.progress_bar.total = self.largo_inicial
+        self.film_displayed = choice(self.films_list)
+        self.film_title.update("[b]"+self.film_displayed)
+
+    def set_list(self, url):
+        self.films_list += get_list_content(url)
+        self.largo_inicial = len(self.films_list)
+        self.vistas = 0
+        self.progress_bar.total = self.largo_inicial
+        self.film_displayed = choice(self.films_list)
+        self.film_title.update("[b]"+self.film_displayed)
+
+
+if __name__ == "__main__":
+    Application().run()
---
+++ modulos/main.py
@@ -0,0 +1,157 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
+import os
+from functools import partial
+
+def get_user_page(user, page):
+    url = f"https://letterboxd.com/{user}/watchlist/page/{page}/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    titulos = []
+    ul_element = soup.find('ul', class_=lambda x: x and 'poster-list' in x.split())
+
+    if ul_element:
+        # Encuentra todos los elementos li dentro del elemento ul
+        li_elements = ul_element.find_all('li')
+
+        # Itera sobre los elementos li
+        for li_element in li_elements:
+            div_element = li_element.find('div')
+            img_element = div_element.find('img')
+            titulos.append(img_element.get("alt"))
+
+        return titulos
+    else:
+        raise RuntimeError
+    
+def get_page(url, page):
+    url += f"/page/{page}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    ul_element = soup.find('ul', class_=lambda x: x and 'poster-list' in x.split())
+    titulos = []
+    if ul_element:
+        # Encuentra todos los elementos li dentro del elemento ul
+        li_elements = ul_element.find_all('li')
+
+        # Itera sobre los elementos li
+        for li_element in li_elements:
+            div_element = li_element.find('div')
+            img_element = div_element.find('img')
+            titulos.append(img_element.get("alt"))
+        return titulos
+    else:
+        raise RuntimeError
+
+def get_user_watchlist(user):
+    url = f"https://letterboxd.com/{user}/watchlist/"
+    # print("URL:", url)
+    response = requests.get(url)
+
+
+    # Verifica si la solicitud fue exitosa (código de estado 200)
+    if response.status_code != 200:
+        raise RuntimeError
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        pagination_div = soup.find('div', class_=lambda x: x and 'pagination' in x.split())
+        num_paginas = int(pagination_div.find_all('li')[-1].get_text())
+
+        num_hilos = min(8, os.cpu_count() * 2)
+        num_hilos = os.cpu_count() * 2
+
+        get_page_from_user = partial(get_user_page, user)
+        resultados = {}
+        with ThreadPoolExecutor(max_workers=num_hilos) as executor:
+            futures = [executor.submit(get_page_from_user, i) for i in range(1, num_paginas + 1)]
+
+            # Utilizamos as_completed para obtener los resultados a medida que se completan
+            for future in as_completed(futures):
+                try:
+                    result = future.result()
+                    resultados[future] = result
+                except Exception as e:
+                    # Manejar excepciones si es necesario
+                    print(f"Error: {e}")
+
+        retorno = list(resultados.values())
+        retorno = [titulo for lista in retorno for titulo in lista]
+        return retorno
+
+    
+def get_list_content(url):
+    response = requests.get(url)
+
+    # Verifica si la solicitud fue exitosa (código de estado 200)
+    retorno = []
+    if response.status_code != 200:
+        raise RuntimeError
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        pagination_div = soup.find('div', class_=lambda x: x and 'pagination' in x.split())
+        if pagination_div != None:
+            num_paginas = int(pagination_div.find_all('li')[-1].get_text())
+        else:
+            num_paginas = 1
+
+        num_hilos = min(8, os.cpu_count() * 2)
+        num_hilos = os.cpu_count() * 2
+
+        get_page_from_list = partial(get_page, url)
+        # with ThreadPoolExecutor(max_workers=num_hilos) as executor:
+        #     executor.map(get_page_from_list, range(1, num_paginas + 1))
+
+        resultados = {}
+        with ThreadPoolExecutor(max_workers=num_hilos) as executor:
+            futures = [executor.submit(get_page_from_list, i) for i in range(1, num_paginas + 1)]
+
+            # Utilizamos as_completed para obtener los resultados a medida que se completan
+            for future in as_completed(futures):
+                try:
+                    result = future.result()
+                    resultados[future] = result
+                except Exception as e:
+                    # Manejar excepciones si es necesario
+                    print(f"Error: {e}")
+
+
+        retorno = list(resultados.values())
+        retorno = [titulo for lista in retorno for titulo in lista]
+        return retorno
+    
+def existe_usuario(user):
+    url = f"https://letterboxd.com/{user}"
+    # print("URL:", url)
+    response = requests.get(url)
+
+    return response.status_code == 200
+
+def existe_lista(url):
+    return requests.get(url).status_code == 200
+
+
+            
+if __name__ == "__main__":
+    # users = ["eliseobndtti", "Justo14", "atylb"]
+    # # users = ["Justo14"]
+    # for user in users:
+    #     t_inicio = time.time()
+    #     retorno = get_user_watchlist(user)
+    #     t_fin = time.time()
+    #     print(user, len(retorno), f"Tardó {t_fin-t_inicio} segundos")
+    #     # print(retorno)
+
+    # # retorno = get_list_content("https://letterboxd.com/eliseobndtti/list/ciclo-de-verano-cinefilo-con-mi-amigo-atilio/")
+    # # print(retorno)
+
+    print(existe_usuario("Justo14"))
+
+
+    
+
